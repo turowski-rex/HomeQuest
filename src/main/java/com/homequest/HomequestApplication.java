38c2e703308f4ad4df2f5e86c6ac65956ea1406a
turowski-rex <153832445+turowski-rex@users.noreply.github.com>
2025-05-04 13:40:39 +0200

    Clean-up of structure
    Changed locations in folders and repetitions

---
+++ src/main/java/com/homequest/HomequestApplication.java
@@ -0,0 +1,12 @@
+package com.homequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class HomequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomequestApplication.class, args);
+    }
+}
